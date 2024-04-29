d13540b8ca2c5f5264e2f6c3c92a249724492ffe
zflorezz <zakinahf030904@gmail.com>
2024-04-28 21:15:05 -0500

    finished

--- src/views/MealsByIngredients.vue
+++ src/views/MealsByIngredients.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { onMounted, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import store from '../store'
 import Meals from '../components/Meals.vue'
 
 const meals = computed(() => store.state.mealsByIngredient)
-const ingredient = computed(() => store.state.ingredient)
+
 const route = useRoute()
 
 onMounted(() => {
