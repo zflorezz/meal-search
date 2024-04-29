f9ce37ecc77ff2ebe58fb7e0001cdb43e55d5385
zflorezz <zakinahf030904@gmail.com>
2024-04-28 20:33:22 -0500

    Getting ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,6 +1,25 @@
 <script setup>
 
+import { ref, onMounted, computed } from 'vue'
+import useAPI from '@/composables/useAPI';
+
+const { api } = useAPI()
+
+const keyword = ref('')
+const ingredients = ref([])
+
+onMounted(() => {
+    api.get('list.php?i=list')
+        .then(({ data }) => {
+        ingredients.value = data.meals
+    })
+})
+
 </script>
 <template>
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4"> Search Meals By Ingredients</h1>
+    </div>
 
+    {{ ingredients }}
 </template>
